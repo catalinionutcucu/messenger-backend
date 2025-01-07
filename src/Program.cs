29794a3b864cbbeb675a392a301a8ee0b77b325c
catalin <catalinionut.cucu@gmail.com>
2025-01-07 12:07:41 +0200

    Initial commit: configured Serilog

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+using Messenger.Handlers;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+});
+
+var app = builder.Build();
+
+app.UseSerilogRequestLogging();
+
+app.Run();
