c1ca8840b8e78c430d7c5d76b23ec75b2817d31c
catalin <catalinionut.cucu@gmail.com>
2025-01-07 12:21:16 +0200

    Feature: added result object
    The result object will be used to represent the result (success or failure) of a function.

---
+++ src/Extensions/ResultExtensions.cs
@@ -0,0 +1,113 @@
+﻿using Messenger.Models;
+
+namespace Messenger.Extensions;
+
+#region value result
+
+public static partial class ResultExtensions
+{
+    /// <summary>
+    /// Maps the result to a value of type <c>TMap</c> by executing the corresponding function based on whether the result is success or failure
+    /// </summary>
+    /// <returns>The value of type <c>TMap</c> based on whether the result is success or failure</returns>
+    public static TMap Map<TMap, TResult, TError>(this Result<TResult, TError> result, Func<TResult, TMap> success, Func<TError, TMap> failure)
+    {
+        return result.IsSuccess ? success(result.Value) : failure(result.Error);
+    }
+
+    /// <summary>
+    /// Maps the result to a value of type <c>TMap</c> by executing the corresponding asynchronous function based on whether the result is success or failure
+    /// </summary>
+    /// <returns>The value of type <c>TMap</c> based on whether the result is success or failure</returns>
+    public static async Task<TMap> MapAsync<TMap, TResult, TError>(this Result<TResult, TError> result, Func<TResult, Task<TMap>> success, Func<TError, Task<TMap>> failure)
+    {
+        return result.IsSuccess ? await success(result.Value) : await failure(result.Error);
+    }
+
+    /// <summary>
+    /// Executes the corresponding function based on whether the result is success or failure
+    /// </summary>
+    public static void Execute<TResult, TError>(this Result<TResult, TError> result, Action<TResult> success, Action<TError> failure)
+    {
+        if (result.IsSuccess)
+        {
+            success(result.Value);
+        }
+        else
+        {
+            failure(result.Error);
+        }
+    }
+
+    /// <summary>
+    /// Executes the corresponding asynchronous function based on whether the result is success or failure
+    /// </summary>
+    public static async Task ExecuteAsync<TResult, TError>(this Result<TResult, TError> result, Func<TResult, Task> success, Func<TError, Task> failure)
+    {
+        if (result.IsSuccess)
+        {
+            await success(result.Value);
+        }
+        else
+        {
+            await failure(result.Error);
+        }
+    }
+}
+
+#endregion
+
+#region empty result
+
+public static partial class ResultExtensions
+{
+    /// <summary>
+    /// Maps the result to a value of type <c>TMap</c> by executing the corresponding function based on whether the result is success or failure
+    /// </summary>
+    /// <returns>The value of type <c>TMap</c> based on whether the result is success or failure</returns>
+    public static TMap Map<TMap, TError>(this Result<TError> result, Func<TMap> success, Func<TError, TMap> failure)
+    {
+        return result.IsSuccess ? success() : failure(result.Error);
+    }
+
+    /// <summary>
+    /// Maps the result to a value of type <c>TMap</c> by executing the corresponding asynchronous function based on whether the result is success or failure
+    /// </summary>
+    /// <returns>The value of type <c>TMap</c> based on whether the result is success or failure</returns>
+    public static async Task<TMap> MapAsync<TMap, TError>(this Result<TError> result, Func<Task<TMap>> success, Func<TError, Task<TMap>> failure)
+    {
+        return result.IsSuccess ? await success() : await failure(result.Error);
+    }
+
+    /// <summary>
+    /// Executes the corresponding function based on whether the result is success or failure
+    /// </summary>
+    public static void Execute<TError>(this Result<TError> result, Action success, Action<TError> failure)
+    {
+        if (result.IsSuccess)
+        {
+            success();
+        }
+        else
+        {
+            failure(result.Error);
+        }
+    }
+
+    /// <summary>
+    /// Executes the corresponding asynchronous function based on whether the result is success or failure
+    /// </summary>
+    public static async Task ExecuteAsync<TError>(this Result<TError> result, Func<Task> success, Func<TError, Task> failure)
+    {
+        if (result.IsSuccess)
+        {
+            await success();
+        }
+        else
+        {
+            await failure(result.Error);
+        }
+    }
+}
+
+#endregion
---
+++ src/Models/Result.cs
@@ -0,0 +1,113 @@
+﻿namespace Messenger.Models;
+
+#region value result
+
+public class Result<TResult, TError>
+{
+    public ResultState State { get; }
+
+    public TResult? Value { get; }
+
+    public TError? Error { get; }
+
+    public bool IsSuccess => State is ResultState.Success;
+
+    public bool IsFailure => State is ResultState.Failure;
+
+    protected Result(TResult value)
+    {
+        State = ResultState.Success;
+        Value = value;
+    }
+
+    protected Result(TError error)
+    {
+        State = ResultState.Failure;
+        Error = error;
+    }
+
+    /// <summary>
+    /// Creates a <see cref = "Result{TResult, TError}" /> instance representing a success result
+    /// </summary>
+    /// <returns>A <see cref = "Result{TResult, TError}" /> instance representing a success result</returns>
+    public static Result<TResult, TError> Success(TResult value)
+    {
+        return new(value);
+    }
+
+    /// <summary>
+    /// Creates a <see cref = "Result{TResult, TError}" /> instance representing a failure result
+    /// </summary>
+    /// <returns>A <see cref = "Result{TResult, TError}" /> instance representing a failure result</returns>
+    public static Result<TResult, TError> Failure(TError error)
+    {
+        return new(error);
+    }
+
+    public static implicit operator Result<TResult, TError>(TResult value)
+    {
+        return Success(value);
+    }
+
+    public static implicit operator Result<TResult, TError>(TError error)
+    {
+        return Failure(error);
+    }
+}
+
+#endregion
+
+#region empty result
+
+public class Result<TError>
+{
+    public ResultState State { get; }
+
+    public TError? Error { get; }
+
+    public bool IsSuccess => State is ResultState.Success;
+
+    public bool IsFailure => State is ResultState.Failure;
+
+    protected Result()
+    {
+        State = ResultState.Success;
+    }
+
+    protected Result(TError error)
+    {
+        State = ResultState.Failure;
+        Error = error;
+    }
+
+    /// <summary>
+    /// Creates a <see cref = "Result{TError}" /> instance representing a success result
+    /// </summary>
+    /// <returns>A <see cref = "Result{TError}" /> instance representing a success result</returns>
+    public static Result<TError> Success()
+    {
+        return new();
+    }
+
+    /// <summary>
+    /// Creates a <see cref = "Result{TError}" /> instance representing a failure result
+    /// </summary>
+    /// <returns>A <see cref = "Result{TError}" /> instance representing a failure result</returns>
+    public static Result<TError> Failure(TError error)
+    {
+        return new(error);
+    }
+
+    public static implicit operator Result<TError>(TError error)
+    {
+        return Failure(error);
+    }
+}
+
+#endregion
+
+public enum ResultState
+{
+    Success,
+    Failure
+}
